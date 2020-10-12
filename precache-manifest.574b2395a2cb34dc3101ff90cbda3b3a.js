e4ec98174881f7f321d68bfb170cd125607a11c5
ITVash <vash_dn@mail.ru>
2020-10-12 16:57:29 +0300

    first commit

---
+++ precache-manifest.574b2395a2cb34dc3101ff90cbda3b3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66bf27a5d1abbd6e0c7771b7e096194",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ae0fedf6ec344d88a02",
+    "url": "/static/css/main.0c0d4868.chunk.css"
+  },
+  {
+    "revision": "0655b7ae2d1a9370a16d",
+    "url": "/static/js/2.db22d39c.chunk.js"
+  },
+  {
+    "revision": "53459d4c6bcff25c7a60c9ccd7947f41",
+    "url": "/static/js/2.db22d39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae0fedf6ec344d88a02",
+    "url": "/static/js/main.17bca38f.chunk.js"
+  },
+  {
+    "revision": "dc7ac3a8c481f3ceea28",
+    "url": "/static/js/runtime-main.39d65a9d.js"
+  }
+]);
